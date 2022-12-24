f280215b50385860e956c0bb1c92d02c91fcaa0d
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-25 00:32:34 +0100

    commit zero

---
+++ Shark Bite Script pastebin roblox SharkBite GUI .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet("https://pastebin.com/raw/YYVLbzVg", true))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Working Easy-Exploits! / In the GUI there is an auto farm, pass through walls, instant kill sharks and more!
+						
+]]
